d5ffc20fbc72462c82d71bfa067a1c27041803ab
Prajwal100 <prajwal.iar@gmail.com>
2023-06-24 11:16:44 +0545

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { typeOrmConfigAsync } from './config/typeorm.config';
+
+@Module({
+  imports: [
+  ConfigModule.forRoot(),
+  TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/typeorm/User.ts
@@ -0,0 +1,36 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum STATUS{
+    ACTIVE='active',
+    INACTIVE='inactive'
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ nullable: false, unique: true })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ nullable: true, name: 'img_url' })
+  imgUrl: string;
+
+  @Column({ nullable: false })
+  role: string;
+  
+  @Column({type:'enum', enum:STATUS,default:STATUS.ACTIVE})
+  status:STATUS
+
+  @Column({ nullable: false, type: 'datetime', name: 'create_at' })
+  createAt: Date;
+
+  @Column({ nullable: false, type: 'datetime', name: 'update_at' })
+  updateAt: Date;
+}
